bc35bf0397afd1144ff7d9b9c135d98c3c807972
YohanDavion <yohandavion31@gmail.com>
2024-04-10 19:17:33 +0200

    rework(All): Changement d'organisation du projet et suppresion des composants en erreur

--- front/banque/src/app/app.module.ts
+++ front/banque/src/app/app.module.ts
@@ -3,19 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { BankerDashboardComponent } from './banker-dashboard/banker-dashboard.component';
-import { AccountManagementComponent } from './account-management/account-management.component';
-import { TransferManagementComponent } from './transfer-management/transfer-management.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    BankerDashboardComponent,
-    AccountManagementComponent,
-    TransferManagementComponent,
     HomeComponent
   ],
   imports: [
