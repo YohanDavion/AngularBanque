518f2dd242ee29881235c31584511ba87367f374
YohanDavion <yohandavion31@gmail.com>
2024-04-16 15:40:24 +0200

    chore(Front): Update des dépendances

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { ListClientsPageComponent } from './pages/list-clients-page/list-clients-page.component';
 import { CreateClientsPageComponent } from './pages/create-clients-page/create-clients-page.component';
 import { CreateComptesPageComponent } from './pages/create-comptes-page/create-comptes-page.component';
+import { ListComptesPageComponent } from './pages/list-comptes-page/list-comptes-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full'},
   { path: 'accueil', component: HomeComponent },
   { path: 'clients/liste', component: ListClientsPageComponent },
+  { path: 'comptes/liste', component: ListComptesPageComponent },
   { path: 'clients/creer', component: CreateClientsPageComponent },
   { path: 'comptes/creer', component: CreateComptesPageComponent }
 ];
